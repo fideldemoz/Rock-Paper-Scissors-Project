31e12f0e8758d9fce05af8266c5778cd2936831f
Fidel Livino Pedro <fidellplivino98@gmail.com>
2023-05-07 23:42:25 +0200

    Add GUI
    This update will add Graphical User Interface to the previously developed project. The aim of this project are Events and DOM manipulation.

--- main.js
+++ main.js
@@ -1,82 +1,88 @@
-// Rock Scissors paper
-// Created by Fidel
-// Game rule: rock beats scissors, scissors beat paper, paper beats rock
-//-------------------------------------------------------------------------
-function game() {
-	let i = parseInt(0);
-	let options = ['rock', 'scissors', 'paper'];
-	let keepOn;
-	let cScore = parseInt(0);
-	let pScore = parseInt(0);
+const button = document.querySelectorAll('button');
+const rock = button[0];
+const paper = button[1];
+const scissors = button[2];
 
-	
-	
-	do {
-		i++;
-		
-		//------------------var player----------------------------
-		// PROMPT for players choice (1, 2, 3 )
-		let player = prompt('Enter a choice \n 1 = rock .... 2 = scissors .... 3 = paper');
-		if (player == 1) { // each number corresponds to a choice
-			player = 'rock';
-			keepOn = true;
-		} else if (player == 2) {
-			player = 'scissors';
-			keepOn = true;
-		} else if (player == 3) {
-			player = 'paper';
-			keepOn = true;
-		} else {
-			if (player = null || (player != 'rock' || player != 'scissors' || player != 'paper')) {
-				console.log(`Please, enter a valid choice.`);
-				keepOn = false;
-			} // if players choice is not in range, cancel.
-		}
-		// ----------------end player----------------------
-		
-		// ----------------- var computer ----------------------
-		// GENERATE a computer choice
-		function getComputerChoice() {
-			return options[Math.floor(Math.random()*options.length)];
-		}
-		computer = getComputerChoice();
-		//-------------- end computer -------------------------
-		
-		//-----------------the game--------------------------
-		function singlePlay(player, computer) {
-			let _1, _2, _3;
-			_1 = options[0];
-			_2 = options[1];
-			_3 = options[2];
+const results = document.querySelector('.results');
+const paragraphs = document.querySelectorAll('p');
+const msg = paragraphs[0];
+const playerBoard = paragraphs[1];
+const computerBoard = paragraphs[2];
 
-			// the core game
-			if (player == computer) { //		if computer and user have same choice: Tie. Do not keep record
-				return `Tie. ${player} is same as ${computer}`;
-			} else if (
-			(computer == _3 && player == _1) 
-			|| (computer == _1 && player == _2) 
-			|| (computer == _2 && player == _3)) { //		if computer wins: keep record +1 to computer
-				cScore += 1;
-				return `You lost. ${computer} beats ${player}.
-				Computer ${cScore} - You ${pScore}`
-			} else if ((computer == _1 && player == _3) 
-			|| (player == _1 && computer == _2)
-			|| (player == _2 && computer == _3)){ //		if player wins: keep record +1 to player
-				pScore += 1;
-				return `You won. ${player} beats ${computer}. 
-				Computer ${cScore} - You ${pScore}`
-			}
-		}
-		
-		console.log(`Player: ${player}`);
-		console.log(`Computer: ${computer}`);
-		
-		if (keepOn==true) {
-		console.log(singlePlay(player, computer));
-	}
-	} while (i<5);
-	
-	console.log(`Total
-Computer: ${cScore} - ${pScore} : You`);
-	
+const options = ['rock', 'scissors', 'paper'];
+
+let computerScore = parseInt(0);
+let playerScore = parseInt(0);
+
+function singlePlay(player, computer) {
+	let _1, _2, _3;
+	_1 = options[0];
+	_2 = options[1];
+	_3 = options[2];
+
+
+	if (player == computer) {
+		msg.textContent = `Tie. ${player} is same as ${computer}`;
+		showBoard()
+		checkGame()
+	} else if (
+	(computer == _3 && player == _1) 
+	|| (computer == _1 && player == _2) 
+	|| (computer == _2 && player == _3)) { //		if computer wins: keep record +1 to computer
+		computerScore += 1;
+		msg.textContent = `You lost. ${computer} beats ${player}.`;
+		showBoard()
+		checkGame()
+	} else if ((computer == _1 && player == _3) 
+	|| (player == _1 && computer == _2)
+	|| (player == _2 && computer == _3)){ //		if player wins: keep record +1 to player
+		playerScore += 1;
+		msg.textContent = `You won. ${player} beats ${computer}.`;
+		showBoard()
+		checkGame()
+	} 
 }
+
+function showBoard() {
+	playerBoard.textContent = `You - ${playerScore}`;
+	computerBoard.textContent =  `Computer - ${computerScore}`;
+}
+
+function checkGame() {
+	if (computerScore === 5) {
+		msg.textContent = `Game over. You lost!`;
+		showBoard()
+		playerScore = 0;
+		computerScore = 0;
+} else if (playerScore === 5) {
+		msg.textContent = `Game over. You won!`;
+		showBoard()
+		playerScore = 0;
+		computerScore = 0;
+	}
+}
+
+function getComputerSelection() {
+			return options[Math.floor(Math.random()*options.length)];
+}
+let computerSelection;
+let playerSelection;
+
+rock.addEventListener('click', () => {
+			playerSelection = options[0];
+			computerSelection = getComputerSelection();
+			singlePlay(playerSelection, computerSelection);
+		});
+		
+paper.addEventListener('click', () => {
+			playerSelection = options[1];
+			computerSelection = getComputerSelection();
+			singlePlay(playerSelection, computerSelection);
+		});
+		
+scissors.addEventListener('click', () => {
+			playerSelection = options[2];
+			computerSelection = getComputerSelection();
+			singlePlay(playerSelection, computerSelection);
+		});
+
